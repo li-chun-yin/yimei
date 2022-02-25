671faa95df6c087092a671593205051a545bb633
lichunyin <licy2013@aliyun.com>
2022-02-25 08:20:36 +0800

    Debug video status

--- server/library/model/UploadSyncDescStatus.php
+++ server/library/model/UploadSyncDescStatus.php
@@ -10,25 +10,26 @@ use model\uploadSync\Repository AS UploadSyncRepository;
 use model\uploadSync\Code AS UploadSyncCode;
 use model\uploadSyncDesc\Code AS UploadSyncDescCode;
 use model\uploadSyncDesc\Manager AS UploadSyncDescManager;
+use model\kuaishouId\Repository AS KuaishouIdRepository;
 
 /**
- * 
+ *
  * @author 李春寅 <http://licy.top>
  */
 class UploadSyncDescStatus
 {
     use ContainerAwareTrait;
-    
+
     /**
      * 核对 $UploadSyncDescEntity 的 status 值。并且调用数据更新方法。
      * 但如果更新的数据需要持久化，那么需要在这个方法调用后，继续调用database的flush方法
-     *  - 1. 如果 没有 UploadSync 数据， 
+     *  - 1. 如果 没有 UploadSync 数据，
      *       那么 UploadSyncDesc 的状态是 UploadSyncDescCode::STATUS_WAIT
-     *  - 2. 如果 UploadSync 数据含有 UploadSyncCode::STATUS_ING， 
+     *  - 2. 如果 UploadSync 数据含有 UploadSyncCode::STATUS_ING，
      *  -    那么 UploadSyncDesc 的状态是 UploadSyncDescCode::STATUS_ING
      *  - 3. 如果 UploadSync 数据全部是 UploadSyncCode::STATUS_DONE、数据总数少于绑定系统的第三方账号
      *       那么 UploadSyncDesc 的状态是 UploadSyncDescCode::STATUS_PART
-     *  - 4. 如果 UploadSync 数据全部是 UploadSyncCode::STATUS_DONE、数据总数等于或对于（可能会有第三方账号绑定后删除）绑定系统的第三方账号 
+     *  - 4. 如果 UploadSync 数据全部是 UploadSyncCode::STATUS_DONE、数据总数等于或对于（可能会有第三方账号绑定后删除）绑定系统的第三方账号
      *       那么 UploadSyncDesc 的状态是 UploadSyncDescCode::STATUS_DONE
      * @param UploadSyncDescEntity $UploadSyncDescEntity
      */
@@ -44,9 +45,14 @@ class UploadSyncDescStatus
         $sync_open_ids          = $this->getSyncOpenIds();
         $sync_toutiao_open_ids  = $this->getSyncToutiaoOpenIds();
         $sync_xigua_open_ids    = $this->getSyncXiguaOpenIds();
-        $sync_total             = count($sync_open_ids) + count($sync_toutiao_open_ids) + count($sync_xigua_open_ids);
+        $sync_kuaishou_open_ids = $this->getSyncKuaishouOpenIds();
+        $sync_total             = count($sync_open_ids)
+                                + count($sync_toutiao_open_ids)
+                                + count($sync_xigua_open_ids)
+                                + count($sync_kuaishou_open_ids)
+        ;
         $UploadSyncDescManager->load($UploadSyncDescEntity);
-        
+
         $status                 = UploadSyncDescCode::STATUS_WAIT;
         $synced_count           = 0;
         $UploadSyncEntitys      = $UploadSyncRepository->findByUploadId($UploadSyncDescEntity->getUploadId());
@@ -55,33 +61,40 @@ class UploadSyncDescStatus
                 $status = UploadSyncDescCode::STATUS_ING;
                 break;
             }
-            
+
             if(     $UploadSyncEntity->getType() == UploadSyncCode::TYPE_DOUYIN
                 &&  $UploadSyncEntity->getStatus() == UploadSyncCode::STATUS_DONE
-                &&  in_array($UploadSyncEntity->getUnikey(), $sync_open_ids
-            )){
+                &&  in_array($UploadSyncEntity->getUnikey(), $sync_open_ids)
+            ){
                 $synced_count ++;
             }
-            
+
             if(     $UploadSyncEntity->getType() == UploadSyncCode::TYPE_TOUTIAO
                 &&  $UploadSyncEntity->getStatus() == UploadSyncCode::STATUS_DONE
-                &&  in_array($UploadSyncEntity->getUnikey(), $sync_toutiao_open_ids
-            )){
+                &&  in_array($UploadSyncEntity->getUnikey(), $sync_toutiao_open_ids)
+            ){
                 $synced_count ++;
             }
-            
+
             if(     $UploadSyncEntity->getType() == UploadSyncCode::TYPE_XIGUA
                 &&  $UploadSyncEntity->getStatus() == UploadSyncCode::STATUS_DONE
-                &&  in_array($UploadSyncEntity->getUnikey(), $sync_xigua_open_ids
-            )){
+                &&  in_array($UploadSyncEntity->getUnikey(), $sync_xigua_open_ids)
+            ){
+                $synced_count ++;
+            }
+
+            if(     $UploadSyncEntity->getType() == UploadSyncCode::TYPE_KUAISHOU
+                &&  $UploadSyncEntity->getStatus() == UploadSyncCode::STATUS_DONE
+                &&  in_array($UploadSyncEntity->getUnikey(), $sync_kuaishou_open_ids)
+            ){
                 $synced_count ++;
             }
         }
-        
+
         if($status == UploadSyncDescCode::STATUS_ING) {
             $UploadSyncDescManager->updateStatusIng(['sync_data' => $UploadSyncDescEntity->getSyncData()]);
         }
-        
+
         if($status != UploadSyncDescCode::STATUS_ING && $synced_count > 0 && $synced_count < $sync_total){
             $UploadSyncDescManager->updateStatusPart();
         }
@@ -90,11 +103,11 @@ class UploadSyncDescStatus
             $UploadSyncDescManager->updateStatusDone();
         }
     }
-    
+
     private function getSyncOpenIds()
     {
         /**
-         * 
+         *
          * @var DouyinIdRepository $DouyinIdRepository
          */
         $DouyinIdRepository = $this->Container->get(DouyinIdRepository::class);
@@ -111,7 +124,7 @@ class UploadSyncDescStatus
         $ToutiaoIdRepository = $this->Container->get(ToutiaoIdRepository::class);
         return $ToutiaoIdRepository->getAllOpenIds();
     }
-    
+
     private function getSyncXiguaOpenIds()
     {
         /**
@@ -120,5 +133,15 @@ class UploadSyncDescStatus
          */
         $XiguaIdRepository = $this->Container->get(XiguaIdRepository::class);
         return $XiguaIdRepository->getAllOpenIds();
+    }
+
+    private function getSyncKuaishouOpenIds()
+    {
+        /**
+         *
+         * @var KuaishouIdRepository $KuaishouIdRepository
+         */
+        $KuaishouIdRepository = $this->Container->get(KuaishouIdRepository::class);
+        return $KuaishouIdRepository->getAllOpenIds();
     }
 }
